eb3a85024f7d9fee1d6b659617a67eadf2627d65
david <davidrgmcburney@gmail.com>
2018-03-21 23:33:22 -0700

    Notes and half complete functions for taking and testing guesses added to resizeableDataStructure.c

--- resizeableDataStructure.c
+++ resizeableDataStructure.c
@@ -4,65 +4,52 @@
 #include <time.h>
 #include "wordList.h"
 
-
-#define ARRAY_FILL_START 0
-#define BASE_ARRAY_SIZE 10
-
-void createEmptyWordList(wordList *words) {
-    words->array_filled_num = ARRAY_FILL_START;
-    words->array_size = BASE_ARRAY_SIZE;
-    words->stringArray = malloc(sizeof(char*)*words->array_size);
-}
-
-void expandArray(wordList *words) {
-    words->array_size = (words->array_size * 2);
-    words->stringArray = realloc(words->stringArray, sizeof(char*) * words->array_size);
-}
-
-void addWord(wordList *words, char *buffer) {
-    int temporary = strlen(buffer) + 1;
-    char *temporaryString = malloc(sizeof(char) * temporary);
-    strncpy(temporaryString, buffer, temporary);
-    if (words->array_filled_num == words->array_size) {
-        expandArray(words);
-    }
-    words->stringArray[words->array_filled_num] = strdup(temporaryString);
-}
-
-int wordListLength(wordList words) {
-    return words.array_filled_num;
-}
-
-char * getWord(wordList words, int randomWordAddress) {
-    return words.stringArray[randomWordAddress];
-}
-
-void getWordList(wordList *words) {
-    words->wordListFile = fopen("/home/david/Desktop/hangmanCPort/wordList.txt", "r");
-    char buffer[256];
-    while (fgets(buffer, 256, words->wordListFile) != NULL) {
-        addWord(words, buffer);
-        printf("%d\n", words->array_size);
-        printf("%s\n", words->stringArray[words->array_filled_num]);
-        words->array_filled_num++;
-        }
-}
-
-int main() {
-    time_t startTime;
-    srand((unsigned) time(&startTime));
+void getAnswer() {
+    seedRandomizer();
     wordList words;
     createEmptyWordList(&words);
     getWordList(&words);
-    int listLength = wordListLength(words);
-    int randomWordAddress = rand() % (listLength - 7);
-    printf("%d\n", randomWordAddress);
-    //char *hangmanAnswer = getWord(words, randomWordAddress);
+    listLength = wordListLength(words);
+    randomWordAddress = getRandomAddress(listLength);
+    //printf("%d\n", randomWordAddress);
+    hangmanAnswer = getWord(words, randomWordAddress);
+    //answerLength = strlen(hangmanAnswer);
     //printf("%s\n", hangmanAnswer);
+    fclose(words.wordListFile);
+}//Once answer is determined wordlist is a drain, fclose and free seem prudent but valgrind is reporting unfreed memory when I free(words->possibleAnswers) so definitely need more idea what that free will entail
+
+void getGuess() {
+    hangmanGuess = fgets(hangmanGuess, 2, stdin);//fgets should protect against too much input but extra input seems to curently spill over so may need to hack off everything past first if making the game loop differently doesn't fix
+}
+
+/*void testGuess(hangmanGuess) {
+    //hangmanGuessedCorrect = False;
+    for (int letter = 0; i < (int)(sizeof(hangmanAnswer)); ++letter) {
+        if hangmanGuess == hangmanAnswer[letter] {
+            //hangmanCorrectGuesses[letter] = hangmanGuess; hangmanCorrectGuesses is I guess a dynamically allocated char array
+            //hangmanGuessedCorrect = True; a bool, may need to be more clever, not sure how far this value exists outside scope of this for loop
+        }
+    }
+    //if hangmanGuessedCorrect == False {
+        //hangmanStrikes++; Like in python version, strikes is an int
+}*/
+
+int main() {
+    hangmanGuess = malloc(2);//Move this inside of the gameState whenever that becomes a proper structure
+    int numberOfGuesses = 0;//Just for testing, will go away
+    getAnswer();
+    while (numberOfGuesses <= 4) { //This is off by one somehow, will have to work that out
+        getGuess();
+        printf("%s\n", hangmanGuess);
+        ++numberOfGuesses;
+    }
+    printf("%s\n", hangmanAnswer);
+    printf("%d\n", (int)strlen(hangmanAnswer));
 }
 
 //use strdup, this soultion assigns the entire array pointers to the same thing. OBSOLETE
 
 //why is it a bad idea to pass fgets to strdup? Dangerous when fgets returns a null pointer which it always does when it reaches the EOF. Useful behavior otherwise but in future don't make that one line.
 //TODO: Read from file rather than stdin. DONE
-//TODO: Add into .h file and have other things call it with getters and setters and such applied to the array
+//TODO: Add into .h file and have other things call it with getters and setters and such applied to the array DONE
+//TODO: Function to hack off newline character
--- wordList.h
+++ wordList.h
@@ -1,40 +1,64 @@
 typedef struct {
 FILE *wordListFile;
 char **words;
-char **stringArray;
-int array_size;
-int array_filled_num;
+char **possibleAnswers;
+int arraySize;
+int highestFilledArrayAddress;
 }wordList;
 
-/*#define ARRAY_FILL_START 0
-#define BASE_ARRAY_SIZE 10
+char *hangmanGuess;
+int listLength;
+int randomWordAddress;
+char *hangmanAnswer;//put these in a struct later,gameState maybe
+
+#define ARRAY_FILL_START 0
+#define BASE_arraySize 10
+
+void seedRandomizer() {
+    time_t startTime;
+    srand((unsigned) time(&startTime));
+}//takes the time and uses it to seed rand or else the words get pretty predictable.
 
 void createEmptyWordList(wordList *words) {
-    words->array_filled_num = ARRAY_FILL_START;
-    words->array_size = BASE_ARRAY_SIZE;
-    words->stringArray = malloc(sizeof(char*)*words->array_size);
-}
+    words->highestFilledArrayAddress = ARRAY_FILL_START; //Variable to track how full the array is, necessary given the unclear nature of what file will ofer that list
+    words->arraySize = BASE_arraySize;
+    words->possibleAnswers = malloc(sizeof(char*)*words->arraySize);
+}//Constructs a struct to hold the list of words to be chosen from at random
 
-void expandArray(int *array_size, char **stringArray) {
-    *array_size = *array_size * 2;
-    stringArray = realloc(stringArray, sizeof(char*)**array_size);
-}
+void expandArray(wordList *words) {
+    words->arraySize = (words->arraySize * 2);
+    words->possibleAnswers = realloc(words->possibleAnswers, sizeof(char*) * words->arraySize);
+}//Basic dynamic array resizing setup given the unknown nature of how long wordList can be
 
-void addWord(wordList *words, char *buffer, char **stringArray, int array_filled_num, int array_size) {
-    char *temporaryString = malloc(sizeof(char));
-    strncpy(temporaryString, buffer, strlen(buffer) - 1);
-    if (words->array_filled_num == words->array_size) {
-        expandArray(&words->array_size, words->stringArray);
+void addWord(wordList *words, char *buffer) {
+    int temporary = strlen(buffer) + 1;
+    char *temporaryString = malloc(sizeof(char) * temporary);
+    strncpy(temporaryString, buffer, temporary);
+    if (words->highestFilledArrayAddress == words->arraySize) {
+        expandArray(words);
     }
-    stringArray[array_filled_num] = strdup(temporaryString);
-}
+    words->possibleAnswers[words->highestFilledArrayAddress] = strdup(temporaryString);
+}//Goes down wordList and copies at each newline into the array of possible words
+
+int wordListLength(wordList words) {
+    return words.highestFilledArrayAddress;
+}//Just a getter to allow the number of words currently in the list to be chosen from to be known
+
+int getRandomAddress (int listLength) {
+    return rand() % (listLength - 1);
+}//Gets a random number within the size of the possible words array to allow for selection of a random word
+
+char * getWord(wordList words, int randomWordAddress) {
+    return words.possibleAnswers[randomWordAddress];
+}//takes the random number returned by getRandomAddress and uses it to get a random word
 
 void getWordList(wordList *words) {
-    words->wordListFile = fopen("/home/david/Desktop/hangmanCPort/wordList.txt", "r");
+    words->wordListFile = fopen("/usr/share/dict/words", "r");
     char buffer[256];
     while (fgets(buffer, 256, words->wordListFile) != NULL) {
-        addWord(words, buffer, words->stringArray, words->array_filled_num, words->array_size);
-        printf("%s\n", words->stringArray[words->array_filled_num]);
-        words->array_filled_num++;
+        addWord(words, buffer);
+        //printf("%d\n", words->arraySize);
+        //printf("%s\n", words->possibleAnswers[words->highestFilledArrayAddress]);
+        words->highestFilledArrayAddress++;
         }
-}*/
+}//opens a file and calls addWord to read from it and return a full list of every word presuming each line contains only one
